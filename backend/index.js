42599591b65bdab050d0d1c98301833add79924c
Tanuj Kher <tanujkher15@gmail.com>
2022-12-07 23:34:16 +0530

    database connectivity

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,24 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 const app = express();
-const port = 8080
+const port = process.env.PORT || 8080
+
+// database connection to MongoDB
+const dbPassword = 'SbwyFhqQqDqCg0TR'
+mongoose.Promise = global.Promise
+mongoose.set('strictQuery', true)
+mongoose.connect(`mongodb+srv://TanujKher:${dbPassword}@cluster0.e8swjge.mongodb.net/?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB. This is a listener to indicate the same.')
+})
+
+app.use(bodyParser.urlencoded({extended : true}))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send(`Application is started on port ${port}`)
