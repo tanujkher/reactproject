8fe5a3e61667f89595166dda446a6978cf29aa65
Tanuj Kher <tanujkher15@gmail.com>
2022-12-11 01:56:37 +0530

    changes for listing page

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,25 +1,59 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 import PlayerSingle from './Players/PlayerSingle';
 import PlayerForm from './Players/PlayerForm';
 import PlayerList from './Players/PlayerList';
 
-function App() {
-  return (
-    <div className="container-fluid">
-      <div className='row'>
-        <div className='col s12'>Menu</div>
+class App extends React.Component {
+  constructor(prop) {
+    super(prop)
+    this.state = {
+      players: [],
+      currentPlayer: {}
+    }
+
+    // binding the function for usage in class
+    this.updateCurrentPlayer = this.updateCurrentPlayer.bind(this)
+  }
+
+  componentDidMount(){
+    const url = 'http://localhost:8080/players'
+
+    axios.get(url)
+      .then(Response => {
+        this.setState({
+          players: Response.data
+        })
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  updateCurrentPlayer(item){
+    this.setState({
+      currentPlayer: item
+    })
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className='row'>
+          <div className='col s12'>Menu</div>
+        </div>
+        <div className='row'>
+          <div className='col s3'><PlayerList players={this.state.players} updateCurrentPlayer={this.updateCurrentPlayer} /></div>
+          <div className='col s9'><PlayerSingle /></div>
+        </div>
+        <div className='row'>
+          <div className='col s12'><PlayerForm /></div>
+        </div>
       </div>
-      <div className='row'>
-        <div className='col s3'><PlayerList /></div>
-        <div className='col s9'><PlayerSingle /></div>
-      </div>
-      <div className='row'>
-        <div className='col s12'><PlayerForm /></div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- frontend/src/Components/Players/PlayerList.js
+++ frontend/src/Components/Players/PlayerList.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const PlayerList = () => {
-    return ( <div>
-        This is Player List
-    </div> );
+const PlayerList = (prop) => {
+    return (
+        <div>
+            <ul className='collection with-header'>
+                <li className='collection-header'><h4>Players</h4></li>
+                {prop.players.map((item) => (
+                    <a href='#!' className='collection-item' onClick={prop.updateCurrentPlayer.bind(this, item)} key={item._id}>{item.firstName}</a>
+                ))}
+            </ul>
+        </div>
+    );
 }
- 
+
 export default PlayerList;
