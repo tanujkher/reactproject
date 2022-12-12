80b8ef64b0e51c36531af19d583788e96e9d88af
Tanuj Kher <tanujkher15@gmail.com>
2022-12-12 15:47:55 +0530

    Form to add users

--- frontend/src/Components/Players/PlayerForm.js
+++ frontend/src/Components/Players/PlayerForm.js
@@ -1,15 +1,54 @@
 import React from 'react';
+import axios from 'axios';
 
 class PlayerForm extends React.Component {
-    constructor(props) {
-        super(props);
+    onSubmitPlayer(event) {
+        event.preventDefault();
+        axios.post('http://localhost:8080/players', {
+            firstName: this.refs.firstName.value,
+            lastName: this.refs.lastName.value,
+            phone: this.refs.phone.value,
+            email: this.refs.email.value
+        }).then((response) => {
+            console.log(response)
+        }).catch((error) => {
+            console.log(error)
+        })
     }
-    state = {  }
-    render() { 
-        return ( <div>
-            This is Player Form
-        </div> );
+    render() {
+        return (
+            <div className="row">
+                <h4 className='center'>Add a new Team Member</h4>
+                <form className="col s12 center" onSubmit={this.onSubmitPlayer.bind(this)}>
+                    <div className="row">
+                        <div className="input-field col s2"></div>
+                        <div className="input-field col s4">
+                            <input id="firstName" ref="firstName" type="text" />
+                            <label>First Name</label>
+                        </div>
+                        <div className="input-field col s4">
+                            <input id="lastName" ref="lastName" type="text" />
+                            <label>Last Name</label>
+                        </div>
+                        <div className="input-field col s2"></div>
+                    </div>
+                    <div className="row">
+                        <div className="input-field col s2"></div>
+                        <div className="input-field col s4">
+                            <input id="phone" ref="phone" type="text" />
+                            <label>Phone</label>
+                        </div>
+                        <div className="input-field col s4">
+                            <input id="email" ref="email" type="text" />
+                            <label>Email</label>
+                        </div>
+                        <div className="input-field col s2"></div>
+                    </div>
+                    <button className='btn waves-effect waves-light center' type='submit' name='action'>SUBMIT</button>
+                </form>
+            </div>
+        );
     }
 }
- 
+
 export default PlayerForm;
